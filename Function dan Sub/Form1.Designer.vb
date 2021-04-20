7a434390f8da10fbd7cad8e99ac92b2e9a18450a
Nitasha Martin <shukshara09@gmail.com>
2021-04-20 23:21:06 +0800

    function dan sub

--- Function dan Sub/Form1.Designer.vb
+++ Function dan Sub/Form1.Designer.vb
@@ -23,125 +23,130 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btn1 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.txtbox1 = New System.Windows.Forms.TextBox()
+        Me.txtbox2 = New System.Windows.Forms.TextBox()
         Me.btn2 = New System.Windows.Forms.Button()
         Me.btn3 = New System.Windows.Forms.Button()
         Me.btn4 = New System.Windows.Forms.Button()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.lbl4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btn1
         '
+        Me.btn1.BackColor = System.Drawing.Color.CadetBlue
         Me.btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn1.Location = New System.Drawing.Point(325, 67)
         Me.btn1.Name = "btn1"
         Me.btn1.Size = New System.Drawing.Size(85, 37)
         Me.btn1.TabIndex = 0
         Me.btn1.Text = "CLICK"
-        Me.btn1.UseVisualStyleBackColor = True
+        Me.btn1.UseVisualStyleBackColor = False
         '
-        'Label1
+        'lbl1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(511, 77)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(65, 20)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Label1"
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl1.Location = New System.Drawing.Point(492, 74)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(65, 20)
+        Me.lbl1.TabIndex = 1
+        Me.lbl1.Text = "Label1"
         '
-        'Label2
+        'lbl2
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(511, 154)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(65, 20)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Label2"
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl2.Location = New System.Drawing.Point(492, 151)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(65, 20)
+        Me.lbl2.TabIndex = 2
+        Me.lbl2.Text = "Label2"
         '
-        'TextBox1
+        'txtbox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(146, 151)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(155, 22)
-        Me.TextBox1.TabIndex = 3
+        Me.txtbox1.Location = New System.Drawing.Point(120, 151)
+        Me.txtbox1.Name = "txtbox1"
+        Me.txtbox1.Size = New System.Drawing.Size(155, 22)
+        Me.txtbox1.TabIndex = 3
         '
-        'TextBox2
+        'txtbox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(146, 336)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(155, 22)
-        Me.TextBox2.TabIndex = 4
+        Me.txtbox2.Location = New System.Drawing.Point(120, 336)
+        Me.txtbox2.Name = "txtbox2"
+        Me.txtbox2.Size = New System.Drawing.Size(155, 22)
+        Me.txtbox2.TabIndex = 4
         '
         'btn2
         '
+        Me.btn2.BackColor = System.Drawing.Color.CadetBlue
         Me.btn2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn2.Location = New System.Drawing.Point(325, 144)
         Me.btn2.Name = "btn2"
         Me.btn2.Size = New System.Drawing.Size(85, 36)
         Me.btn2.TabIndex = 5
         Me.btn2.Text = "CLICK"
-        Me.btn2.UseVisualStyleBackColor = True
+        Me.btn2.UseVisualStyleBackColor = False
         '
         'btn3
         '
+        Me.btn3.BackColor = System.Drawing.Color.CadetBlue
         Me.btn3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn3.Location = New System.Drawing.Point(325, 236)
         Me.btn3.Name = "btn3"
         Me.btn3.Size = New System.Drawing.Size(85, 35)
         Me.btn3.TabIndex = 6
         Me.btn3.Text = "CLICK"
-        Me.btn3.UseVisualStyleBackColor = True
+        Me.btn3.UseVisualStyleBackColor = False
         '
         'btn4
         '
+        Me.btn4.BackColor = System.Drawing.Color.CadetBlue
         Me.btn4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn4.Location = New System.Drawing.Point(325, 324)
         Me.btn4.Name = "btn4"
         Me.btn4.Size = New System.Drawing.Size(85, 41)
         Me.btn4.TabIndex = 7
         Me.btn4.Text = "CLICK"
-        Me.btn4.UseVisualStyleBackColor = True
+        Me.btn4.UseVisualStyleBackColor = False
         '
-        'Label3
+        'lbl3
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(514, 245)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(65, 20)
-        Me.Label3.TabIndex = 8
-        Me.Label3.Text = "Label3"
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl3.Location = New System.Drawing.Point(492, 242)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(65, 20)
+        Me.lbl3.TabIndex = 8
+        Me.lbl3.Text = "Label3"
         '
-        'Label4
+        'lbl4
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(514, 336)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(65, 20)
-        Me.Label4.TabIndex = 9
-        Me.Label4.Text = "Label4"
+        Me.lbl4.AutoSize = True
+        Me.lbl4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl4.Location = New System.Drawing.Point(492, 336)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(65, 20)
+        Me.lbl4.TabIndex = 9
+        Me.lbl4.Text = "Label4"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.BurlyWood
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.lbl3)
         Me.Controls.Add(Me.btn4)
         Me.Controls.Add(Me.btn3)
         Me.Controls.Add(Me.btn2)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtbox2)
+        Me.Controls.Add(Me.txtbox1)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.lbl1)
         Me.Controls.Add(Me.btn1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -151,13 +156,13 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents btn1 As Button
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents txtbox1 As TextBox
+    Friend WithEvents txtbox2 As TextBox
     Friend WithEvents btn2 As Button
     Friend WithEvents btn3 As Button
     Friend WithEvents btn4 As Button
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents lbl4 As Label
 End Class
--- Function dan Sub/Form1.vb
+++ Function dan Sub/Form1.vb
@@ -1,5 +1,40 @@
 ﻿Public Class Form1
     Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
-
+        Papar()
     End Sub
+
+    Private Sub Papar()
+        lbl1.Text = "Quotes"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        Dim strText As String
+        strText = txtbox1.Text
+        lbl2.Text = strText
+        Quotes(strQuotes:=txtbox1.Text)
+    End Sub
+
+    Private Sub Quotes(strQuotes As String)
+        Dim strInsert As String
+    End Sub
+
+    Function paparNama()
+        lbl3.Text = "Quotes By"
+    End Function
+
+    Private Function btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        paparNama()
+    End Function
+
+    Function Quotesby(strQuotesby As String)
+        Dim strInsert2 As String
+    End Function
+
+    Private Function btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        Dim strText2 As String
+        strText2 = txtbox2.Text
+        lbl4.Text = strText2
+        Quotesby(strQuotesby:=txtbox2.Text)
+    End Function
+
 End Class
